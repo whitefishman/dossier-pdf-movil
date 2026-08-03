7582881cc1b272496b055736b5bd8702d9f88c62
whitefishman <bravojorgefernandez@gmail.com>
2026-08-03 06:44:16 +0200

    fix: compactar tarjetas de preparación de envío

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "dossier-pdf-v15";
+const CACHE_NAME = "dossier-pdf-v16";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=15",
-  "./app.js?v=15",
-  "./prepare-send.css?v=15",
-  "./prepare-send.js?v=15",
+  "./app.js?v=16",
+  "./prepare-send.css?v=16",
+  "./prepare-send.js?v=16",
   "./manifest.webmanifest",
   "./icons/dossier-xa-icon.svg",
   "./icons/icon-192.png",
